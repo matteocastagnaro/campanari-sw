f2b10b6a860e64bee0f28791d953a91d7328fbcd
Diletta <Diletta@192.168.0.102>
2015-06-04 23:50:44 +0200

    aggiunta gestione bottoni e sliding del testo

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,9 +1,11 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -12,39 +14,98 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import menubar.MyMenuBar;
-
-
 @SuppressWarnings("serial")
-public class MainFrame extends JFrame{
-	
-	private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();  
+public class MainFrame extends JFrame {
+
+	private final Dimension screenSize = Toolkit.getDefaultToolkit()
+			.getScreenSize();
 
 	private final int SCREEN_WIDTH = 1200;
 	private final int SCREEN_HEIGHT = 300;
 	
-	public MainFrame(){
+	private int SEC = 0;
+
+	private MarqueePanel mp;
+	
+	public MainFrame() {
+
+		String str = "6,5,6,7,8,8,4/5,5,6,7";
+		mp = new MarqueePanel(str, 40);
+
 		setJFrameOnScreen();
 		setTitle("CAMPANARI v1.0");
 		setJMenuBar(MyMenuBar());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setLayout(new BorderLayout());
+
+		JButton startb = new JButton("START");
+		JButton stopb = new JButton("STOP");
+		JButton pauseb = new JButton("PAUSE");
+
+		stopb.setEnabled(false);
+		pauseb.setEnabled(false);
 		
-		String s = "Tomorrow, and tomorrow, and tomorrow, "
-		        + "creeps in this petty pace from day to day, "
-		        + "to the last syllable of recorded time; ... "
-		        + "It is a tale told by an idiot, full of "
-		        + "sound and fury signifying nothing.";
-		        MarqueePanel mp = new MarqueePanel(s, 32);
-		        add(mp);
-		        pack();
-		        setLocationRelativeTo(null);
-		        setVisible(true);
-		        mp.start();
+		JPanel pbuttons = new JPanel();
+		pbuttons.add(startb);
+		pbuttons.add(pauseb);
+		pbuttons.add(stopb);
+
+		add(pbuttons, BorderLayout.SOUTH);
+
+		// String s = "Tomorrow, and tomorrow, and tomorrow, "
+		// + "creeps in this petty pace from day to day, "
+		// + "to the last syllable of recorded time; ... "
+		// + "It is a tale told by an idiot, full of "
+		// + "sound and fury signifying nothing.";
+
+		mp.setText("    " + str);
+		add(mp, BorderLayout.NORTH);
+		pack();
+		setLocationRelativeTo(null);
+
+		startb.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mp.start();
+				startb.setEnabled(false);
+				stopb.setEnabled(true);
+				pauseb.setEnabled(true);
+			}
+		});
+		
+		pauseb.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mp.pause();
+				startb.setEnabled(true);
+				stopb.setEnabled(true);
+				pauseb.setEnabled(false);
+			}
+		});
+
+		stopb.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startb.setEnabled(true);
+				stopb.setEnabled(false);
+				pauseb.setEnabled(false);
+				mp.pause();
+				mp = new MarqueePanel(str, 40);
+				mp.setText("    " + str);
+				add(mp, BorderLayout.NORTH);
+				pack();
+				setLocationRelativeTo(null);
+			}
+		});
 		
 		setVisible(true);
 	}
-	
-	public JMenuBar MyMenuBar(){
+
+	public JMenuBar MyMenuBar() {
 		JMenuBar mb = new JMenuBar();
 		JMenu file = new JMenu("Modifica");
 		JMenuItem vel = new JMenuItem("Velocità");
@@ -56,52 +117,55 @@ public class MainFrame extends JFrame{
 	}
 
 	private void setJFrameOnScreen() {
-		int x = (screenSize.width - SCREEN_WIDTH) / 2;  
+		int x = (screenSize.width - SCREEN_WIDTH) / 2;
 		int y = (screenSize.height - SCREEN_HEIGHT) / 2;
-		setBounds(x,y,SCREEN_WIDTH,SCREEN_HEIGHT);
+		setBounds(x, y, SCREEN_WIDTH, SCREEN_HEIGHT);
 	}
-	
+
 	class MarqueePanel extends JPanel implements ActionListener {
 
-	    private static final int RATE = 12;
-	    private final Timer timer = new Timer(100 / RATE, this);
-	    private final JLabel label = new JLabel();
-	    private final String s;
-	    private final int n;
-	    private int index;
+		// private static final int RATE = 12;
+		private final Timer timer = new Timer(1000 * SEC, this);
+		private final JLabel label = new JLabel();
+		private final String s;
+		private final int n;
+		private int index;
 
-	    public MarqueePanel(String s, int n) {
-	        if (s == null || n < 1) {
-	            throw new IllegalArgumentException("Null string or n < 1");
-	        }
-	        StringBuilder sb = new StringBuilder(n);
-	        for (int i = 0; i < n; i++) {
-	            sb.append(' ');
-	        }
-	        this.s = sb + s + sb;
-	        this.n = n;
-	        label.setFont(new Font("Serif", Font.ITALIC, 36));
-	        label.setText(sb.toString());
-	        this.add(label);
-	    }
+		public MarqueePanel(String s, int n) {
+			if (s == null || n < 1) {
+				throw new IllegalArgumentException("Null string or n < 1");
+			}
+			StringBuilder sb = new StringBuilder(n);
+			for (int i = 0; i < n; i++) {
+				sb.append(' ');
+			}
+			this.s = "        " + "    " + s + sb;
+			this.n = n;
+			label.setFont(new Font("Serif", Font.ITALIC, 72));
+			this.add(label);
+		}
+		
+		public void setText(String s){
+			label.setText(s);
+		}
 
-	    public void start() {
-	        timer.start();
-	    }
+		public void start() {
+			timer.start();
+		}
 
-	    public void stop() {
-	        timer.stop();
-	    }
+		public void pause() {
+			timer.stop();
+		}
 
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-	        index++;
-	        if (index > s.length() - n) {
-	            index = 0;
-	        }
-	        label.setText(s.substring(index, index + n));
-	    }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			index++;
+			if (index > s.length() - n) {
+				index = 0;
+			}
+			label.setText(s.substring(index, index + n));
+		}
 
 	}
-	
+
 }
